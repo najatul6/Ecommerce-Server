380b17cf4b9555a9817242698908af112a32e9ff
Md Najatul Islam <mdnajatulislam.develop@gmail.com>
2024-12-18 07:41:25 +0600

    first commit

---
+++ server.js
@@ -0,0 +1,40 @@
+const express = require('express')
+const mongoose = require("mongoose");
+const cookieParser = require("cookie-parser");
+const cors = require("cors");
+require('dotenv').config()
+
+// create a database connection
+
+mongoose.connect(process.env.MONGODB_URL)
+  .then(() => {
+    console.log("Server is connected to the database");
+  })
+  .catch((error) => {
+    console.log("Error: ", error);
+  });
+
+const app = express()
+const PORT = process.env.PORT || 5000;
+
+app.use(
+  cors({
+    origin: ["http://localhost:5173"],
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    allowedHeaders: [
+      "Content-Type",
+      "Authorization",
+      "cache-control",
+      "expires",
+      "pragma",
+    ],
+    credentials: true,
+  })
+);
+app.use(cookieParser());
+app.use(express.json());
+
+
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
