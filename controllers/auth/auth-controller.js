bd4bfa54ee8fa572d0727b1f05cddeeb91051de7
Md Najatul Islam <mdnajatulislam.develop@gmail.com>
2025-01-06 00:45:33 +0600

    Update registerUser to use userName field instead of username and change response status to 200

--- controllers/auth/auth-controller.js
+++ controllers/auth/auth-controller.js
@@ -4,16 +4,16 @@ const User = require('../../models/User');
 
 // register
 const registerUser = async(req,res) => {
-    const {username, email, password} = req.body;
+    const {userName, email, password} = req.body;
     try{
         const hashedPassword = await bcrypt.hash(password, 12);
         const newUser= new User({
-            username,
+            userName,
             email,
             password: hashedPassword
         })
         await newUser.save();
-        res.status(201).json({
+        res.status(200).json({
             success: true,
             message: "User registered successfully"
         });
