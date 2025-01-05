3f21378d6fd12161965ad4f698a3c2ce0f1cd37e
Md Najatul Islam <mdnajatulislam.develop@gmail.com>
2025-01-05 21:48:13 +0600

    Implement user registration by saving new user to the database with hashed password

--- controllers/auth/auth-controller.js
+++ controllers/auth/auth-controller.js
@@ -7,6 +7,16 @@ const register = async(req,res) => {
     const {username, email, password} = req.body;
     try{
         const hashedPassword = await bcrypt.hash(password, 12);
+        const newUser= new User({
+            username,
+            email,
+            password: hashedPassword
+        })
+        await newUser.save();
+        res.status(201).json({
+            success: true,
+            message: "User registered successfully"
+        });
     }catch(err){
         console.log(err);
         res.status(500).json({
