bd444e896de45964e7fa640e44dc06dcfe76f52d
Md Najatul Islam <mdnajatulislam.develop@gmail.com>
2025-01-09 22:39:12 +0600

    Refactor registerUser to use userName field, add email existence check, and improve response messages

--- controllers/auth/auth-controller.js
+++ controllers/auth/auth-controller.js
@@ -1,31 +1,39 @@
-const bcrypt = require ('bcryptjs');
-const jwt = require('jsonwebtoken');
-const User = require('../../models/User');
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+const User = require("../../models/User");
 
-// register
-const registerUser = async(req,res) => {
-    const {username, email, password} = req.body;
-    try{
-        const hashedPassword = await bcrypt.hash(password, 12);
-        const newUser= new User({
-            username,
-            email,
-            password: hashedPassword
-        })
-        await newUser.save();
-        res.status(200).json({
-            success: true,
-            message: "User registered successfully"
-        });
-    }catch(err){
-        console.log(err);
-        res.status(500).json({
-            success: false,
-            message: "Some error occured"
-        });
-    }
-}
+//register
+const registerUser = async (req, res) => {
+  const { userName, email, password } = req.body;
 
+  try {
+    const checkUser = await User.findOne({ email });
+    if (checkUser)
+      return res.json({
+        success: false,
+        message: "User Already exists with the same email! Please try again",
+      });
+
+    const hashPassword = await bcrypt.hash(password, 12);
+    const newUser = new User({
+      userName,
+      email,
+      password: hashPassword,
+    });
+
+    await newUser.save();
+    res.status(200).json({
+      success: true,
+      message: "Registration successful",
+    });
+  } catch (e) {
+    console.log(e);
+    res.status(500).json({
+      success: false,
+      message: "Some error occured",
+    });
+  }
+};
 
 // login
 const login = async(req,res) => {
--- routes/auth/auth-routes.js
+++ routes/auth/auth-routes.js
@@ -1,7 +1,10 @@
-const express = require('express');
-const { registerUser } = require('../../controllers/auth/auth-controller');
+const express = require("express");
+const {
+  registerUser,
+} = require("../../controllers/auth/auth-controller");
+
 const router = express.Router();
 
-router.post('/register', registerUser);
+router.post("/register", registerUser);
 
 module.exports = router;
